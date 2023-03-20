93432241567b88df7d21d708bb92e4775e6ff250
Aayush <bhaskarsharma64047@gmail.com>
2023-03-20 17:13:55 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+            url = f"https://shorturllink.in/api?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
